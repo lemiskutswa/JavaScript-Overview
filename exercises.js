56ae2e746ed5ce2080320d44ee776ecd2f50b63f
lemiskutswa <kutswalemis16@gmail.com>
2023-07-06 09:01:27 +0300

    exercises

--- exercises.js
+++ exercises.js
@@ -1,23 +1,6 @@
+const product = {
+    name: 'basketball',
+    price: 2099
+};
 
-// // function calculateTax(cost, taxPercent) {
-// //     console.log(cost * taxPercent);
-// // }
-
-// // calculateTax(500, 0.01);//passing an argument (value) into a function
-
-// // calculateTax(800, 0.01);
-
-// // //this functiont takes a parameter
-// // //parameter only exists in a function scope
-
-// let gameLog = []; 
-
-// const gameLogObject = {
-//     wins: 'You won',
-//     loses: 'You Lost',
-//     draws: 'Tied' 
-// };
-
-// Don't use arrays here 
-
-// console.log(gameLog.push(gameLogObject));
+console.log(product);
