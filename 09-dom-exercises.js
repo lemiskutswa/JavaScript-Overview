45b5ee08add7254ca761111d824768e1a533a0dd
lemiskutswa <kutswalemis16@gmail.com>
2023-07-18 21:34:56 +0300

    added event listener function

--- 09-dom-exercises.js
+++ 09-dom-exercises.js
@@ -1,3 +1,18 @@
-console.log(document.querySelector('button'));
+//function that takes the clicks as a parameter
 
-document.querySelector('.js-button2').innerText = '9b done!';
+function headsHandler() {
+    document.querySelector('.message').innerHTML = 'You picked heads';
+}
+
+function tailsHandler() {
+    document.querySelector('.message').innerHTML = 'You picked tails';
+}
+
+function eventHandler(event) {
+    if (event === 'heads') {
+        document.querySelector('.message').innerHTML = 'You picked heads';
+    } else if (event === 'tails') {
+        document.querySelector('.message').innerHTML = 'You picked tails';
+    }
+}
+
