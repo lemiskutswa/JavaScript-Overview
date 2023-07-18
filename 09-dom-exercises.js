f4529d753ec3158b93a69fcb03291aae25f739b2
lemiskutswa <kutswalemis16@gmail.com>
2023-07-18 21:37:27 +0300

    submit input exercise

--- 09-dom-exercises.js
+++ 09-dom-exercises.js
@@ -1,18 +1,12 @@
 //function that takes the clicks as a parameter
 
-function headsHandler() {
-    document.querySelector('.message').innerHTML = 'You picked heads';
-}
 
-function tailsHandler() {
-    document.querySelector('.message').innerHTML = 'You picked tails';
-}
+// function eventHandler(event) {
+//     if (event === 'heads') {
+//         document.querySelector('.message').innerHTML = 'You picked heads';
+//     } else if (event === 'tails') {
+//         document.querySelector('.message').innerHTML = 'You picked tails';
+//     }
+// }
 
-function eventHandler(event) {
-    if (event === 'heads') {
-        document.querySelector('.message').innerHTML = 'You picked heads';
-    } else if (event === 'tails') {
-        document.querySelector('.message').innerHTML = 'You picked tails';
-    }
-}
 
