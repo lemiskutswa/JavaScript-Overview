e1a9844bbd36caf343c52d4e0292be0aafabae39
lemiskutswa <kutswalemis16@gmail.com>
2023-08-09 16:48:08 +0300

    introduction to loops

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,6 +1,11 @@
 const todoList = [];
 
 function addTodo () {
-    const input = document.getElementById('input-field');
-    input.value
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    todoList.push(name);
+
+    console.log(todoList);
+
+    inputElement.value = '';
 }
