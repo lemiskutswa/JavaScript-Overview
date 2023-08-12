405929952895807ea4fbc6cda021daee7c166f0d
lemiskutswa <kutswalemis16@gmail.com>
2023-08-12 07:42:41 +0300

    loops and array exercises

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -1,27 +1,70 @@
-const buttonElement = document.querySelector('.js-button');
+// const buttonElement = document.querySelector('.js-button');
 
- let result = buttonElement.classList.contains('js-button');
+//  let result = buttonElement.classList.contains('js-button');
 
- console.log(result);
 
-const gamingButton = document.querySelector('.gaming-btn');
+// const gamingButton = document.querySelector('.gaming-btn');
 
-function toggleButton() {
-    const checker = gamingButton.classList.contains('is-toggled');
-    if (!checker) {
-        gamingButton.classList.add('is-toggled');
-    } else {
-        gamingButton.classList.remove('is-toggled');
-    }
+// function toggleButton() {
+//     const checker = gamingButton.classList.contains('is-toggled');
+//     if (!checker) {
+//         gamingButton.classList.add('is-toggled');
+//     } else {
+//         gamingButton.classList.remove('is-toggled');
+//     }
     
+// }
+
+// const threeButtons = document.querySelector('.on-off-btns');
+// function techButtons() {
+//     const checkup = threeButtons.classList.contains('is-on');
+//     if(!checkup) {
+//         threeButtons.classList.add('is-on');
+//     } else {
+//         threeButtons.classList.remove('is-on');
+//     }
+// }
+
+// const newArray = [10, 20, 30];
+
+// newArray[2] = 99;
+// console.log(newArray);
+
+
+// const array = [1, 3, 14, 21];
+// function getLastValue() {
+//     const lastIndex = array.length-1;
+//     return array[lastIndex]; 
+// }
+
+// const myfunction = getLastValue();
+
+// console.log(myfunction);
+
+// const swappableArray = [0, 1, 2, 3, 4, 5];
+// function swapArrays() {
+//     const lastIndex = swappableArray.length-1; 
+//     const firstValue = lastIndex; 
+
+//     const value1 = swappableArray[0];
+//     const lastValue = value1; 
+
+//     return swappableArray; 
+// }
+
+// console.log(swapArrays()); 
+
+for (let i = 0; i <=10; i+=2) {
+     console.log(i);   
+} 
+
+for (let i = 5; i >= 0; i --) {
+    console.log(i);
 }
 
-const threeButtons = document.querySelector('.on-off-btns');
-function techButtons() {
-    const checkup = threeButtons.classList.contains('is-on');
-    if(!checkup) {
-        threeButtons.classList.add('is-on');
-    } else {
-        threeButtons.classList.remove('is-on');
-    }
-}
+let i = 0;
+
+while(i<=10) {
+    console.log(i);
+    i+=2;
+}  
